90206da5c3708c9f0bc632d8399d790486a959fd
vasanth-tamil <tamilfantastic@gmail.com>
2023-10-04 12:02:07 +0530

    category controller create

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\CategoryController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/category', function () {
-    return view('admin.category.index');
-});
-
-Route::get('/sub_category', function () {
-    return view('admin.sub_category.index');
-});
-Route::get('/product', function () {
-    return view('admin.product.index');
-});
-Route::get('/orders', function () {
-    return view('admin.orders.index');
-}); 
+Route::get('/category', [CategoryController::class, 'index']);
